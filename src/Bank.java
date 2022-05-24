637a18a87ad4c6aecf9f0aa706bf7fde03fab52c
Satius D <syutkinaai@gmail.com>
2022-05-24 19:16:52 +0300

    Synchronized Object added

--- src/Bank.java
+++ src/Bank.java
@@ -1,35 +1,26 @@
 public class Bank {
-    private static int money = 10000;
-
-    /*
-    public Bank() {
-
-        Thread client1 = new Client();
-        Thread client2 = new Client();
-        Thread client3 = new Client();
-
-        client1.start();
-        client2.start();
-        client3.start();
-
-
-
-        new Client(this, "Client1").start();
-        new Client(this, "Client2").start();
-        new Client(this, "Client3").start();
-
-    }
-    */
+    private int money = 10000;
+    private Object lock = new Object();
 
     int getMoney() {
         return money;
     }
 
-    static synchronized void take(int money) {
-        Bank.money -= money;
+    void take(int money) {
+        synchronized (lock) {
+            this.money -= money;
+        }
     }
 
-    static synchronized void repay(int money) {
-        Bank.money += money;
+    void repay(int money) {
+        synchronized (lock) {
+            this.money += money;
+        }
+    }
+
+    public Bank() {
+        new Client(this, "Client1").start();
+        new Client(this, "Client2").start();
+        new Client(this, "Client3").start();
     }
 }
